8227a2ddd868e2d902b81274618239ca999bb2e3
mgrvp <mohgp855@gmail.com>
2019-02-14 10:32:27 +0700

    tambah toast

--- app/src/main/java/com/example/windows10/myapp/MainActivity.java
+++ app/src/main/java/com/example/windows10/myapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.windows10.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,30 +15,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestart() {
         super.onRestart();
+        Toast.makeText(getApplicationContext(), "onRestart",Toast.LENGTH_SHORT).show;
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Toast.makeText(getApplicationContext(), "onStart",Toast.LENGTH_SHORT).show;
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Toast.makeText(getApplicationContext(), "onResume",Toast.LENGTH_SHORT).show;
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Toast.makeText(getApplicationContext(), "onPause",Toast.LENGTH_SHORT).show;
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Toast.makeText(getApplicationContext(), "onStop",Toast.LENGTH_SHORT).show;
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Toast.makeText(getApplicationContext(), "onDestroy",Toast.LENGTH_SHORT).show;
     }
 }
